be934f1d2c1be3802d3b546340671a203367d943
Andrea Bizzego <a.bizzego@gmail.com>
2026-09-21 13:44:49 +0200

    first lesson

---
+++ lessons/1.1/teaching.js
@@ -0,0 +1,8 @@
+// OPEN NODE
+// - open terminal
+// - show terminal commands
+// - show node --version
+// - show node
+// - computations with node
+// - console.log
+// - show errors with Syntax
---
+++ lessons/1.2/teaching.js
@@ -0,0 +1,70 @@
+// WORKFLOW 1
+// create folder 
+// create script.js file inside folder
+// write script.js file with blocknotes
+// console.log('Hello Word')
+
+// open terminal
+// ls and cd folder
+// node script.js
+// see result
+
+// change console.log('Hello Andrea')
+// node script.js again
+// observe changed result
+
+// change console.log('Hello Andrea)
+// node script.js again
+// observe error
+
+// WORKFLOW 2
+// create folder
+// open VSCODE
+// open created folder
+// create script.js file
+
+// console.log('Hello Word')
+// observe highlighting
+// run + node.js
+// observe command in terminal
+
+// console.log('Hello Word)
+// observe highlighting
+// run
+// observe error
+
+// consol.log('Hello Word')
+// observe highlighting
+// run
+// observe error
+
+// console.log('Hello Word')
+// breakpoint before line 1
+// see that program pauses
+// see namespace, search console
+
+// STUDENTS - WORKFLOW 2
+// create folder
+// open VSCODE
+// open created folder
+// create script.js file
+
+// console.log('Hello YOURNAME')
+// observe highlighting
+// run + node.js
+// observe command in terminal
+
+// console.log('Hello YOURNAME)
+// observe highlighting indicating an error
+// run
+// observe error
+
+// consol.log('Hello YOURNAME')
+// observe highlighting
+// run
+// observe error
+
+// console.log('Hello YOURNAME')
+// breakpoint before line 1
+// see that program pauses
+// see namespace, search console
